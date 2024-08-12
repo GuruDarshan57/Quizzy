e58ca738678b3c0eadb537212a84380d73764b1b
Gurudarshan57 <venom573116@gmail.com>
2024-08-12 23:15:31 +0530

    minor changes

--- Frontend/src/Pages/Admin/Admin.jsx
+++ Frontend/src/Pages/Admin/Admin.jsx
@@ -28,26 +28,41 @@ const Admin = () => {
         setItem(item + 1)
     }
     return (
-        <div>
-            <div>
-                <form >
-                    <input type="text" placeholder='Question' value={question} onChange={(e) => { setQuestion(e.target.value) }} />
-                    <input type="text" placeholder='Answer' value={answer} onChange={(e) => { setAnswer(e.target.value) }} />
-                    <input type="text" placeholder='Topic' value={topic} onChange={(e) => { setTopic(e.target.value) }} />
-                    <button type="submit" onClick={addItem}>Add</button>
-                </form>
-            </div>
-            {items ? items.map(ele =>
-                <div key={ele.id} style={{ display: "flex", gap: "15px" }}>
-                    <div>{ }</div>
-                    <div>{ele.question}</div>
-                    <div>{ele.answer}</div>
-                    <div><i className="fa-regular fa-pen-to-square"></i></div>
-                    <div><i className="fa-solid fa-trash-can" onClick={() => deleteItem(ele.id)}></i></div>
+        <div className='my-28 w-full flex justify-center'>
+            <div className='w-3/4'>
+                <div className='flex-col py-2'>
+                    <h4 className='font-bold text-3xl ml-4 mb-2'>Add a Question</h4>
+                    <form className='flex'>
+                        <input className='w-56 ml-4 p-1 mr-5 border-2 border-black' type="text" placeholder='Question' value={question} onChange={(e) => { setQuestion(e.target.value) }} />
+                        <input className='w-56 p-1 mr-5 border-2 border-black' type="text" placeholder='Answer' value={answer} onChange={(e) => { setAnswer(e.target.value) }} />
+                        <input className='w-56 p-1 mr-5 border-2 border-black' type="text" placeholder='Topic' value={topic} onChange={(e) => { setTopic(e.target.value) }} />
+                        <button className='border-2 w-32 border-black hover:text-white hover:bg-black' type="submit" onClick={addItem}>Add</button>
+                    </form>
                 </div>
-            ) : ""}
 
-        </div>
+                <div className='flex-col mt-10'>
+                    <h4 className='font-bold text-3xl ml-4 mb-2'>Data Base</h4>
+                    <div className='flex ml-4 mb-4' style={{ display: "flex", gap: "15px" }}>
+
+                        <div className='flex-2 p-1 border-2 border-black'>TOPIC</div>
+                        <div className='flex-1 p-1 border-2 border-black'>QUESTION</div>
+                        <div className='flex-1 p-1 border-2 border-black'>ANSWER</div>
+                        <div className='flex-9 p-1 border-2 border-black'><i className="fa-regular fa-pen-to-square"></i></div>
+                        <div className='flex-9 p-1 border-2 border-black'><i className="fa-solid fa-trash-can" ></i></div>
+                    </div>
+                    {items ? items.map(ele =>
+                        <div className='flex ml-4 mb-4' key={ele.id} style={{ display: "flex", gap: "15px" }}>
+                            <div className='flex-2 border-2 border-black'>{ele.topic}</div>
+                            <div className='flex-1 border-2 border-black'>{ele.question.slice(0, 30)}</div>
+                            <div className='flex-1 border-2 border-black'>{ele.answer.slice(0, 30) + "..."}</div>
+                            <div className='flex-9 border-2 border-black'><i className="fa-regular fa-pen-to-square"></i></div>
+                            <div className='flex-9 border-2 border-black'><i className="fa-solid fa-trash-can" onClick={() => deleteItem(ele.id)}></i></div>
+                        </div>
+                    ) : ""}
+                </div>
+            </div>
+
+        </div >
     )
 }
 
