28291127a038d3446a68f745311034850d7bba14
Gurudarshan57 <venom573116@gmail.com>
2024-08-13 12:12:22 +0530

    admin ui changes

--- Frontend/src/Components/DB_Card.jsx/DB_Card.jsx
+++ Frontend/src/Components/DB_Card.jsx/DB_Card.jsx
@@ -11,19 +11,20 @@ const DB_Card = (props) => {
         const payload = { question: equestion, answer: eanswer, topic: etopic }
         const res = await axios.patch(import.meta.env.VITE_BACKEND + `admin/edit/${props.data.id}`, payload)
         setEdit(false)
+        props.func.f2()
     }
     return (
-        <div className='flex ml-4 mb-4 text-center' style={{ display: "flex", gap: "15px" }}>
+        <div className='flex sm:ml-4 mb-4 text-center' style={{ display: "flex", gap: "15px" }}>
             <input className={`${edit ? "" : "hidden"} w-20 border-2 border-green-700 px-2`} type="text" value={etopic} onChange={(e) => { setEtopic(e.target.value) }} />
-            <div className={`${!edit ? "" : "hidden"} w-20 border-2 border-black`}>{props.data.topic}</div>
+            <div className={`${!edit ? "" : "hidden"} w-20 p-1 sm:p-0 border-2 border-black`}>{props.data.topic}</div>
 
             <input className={`${edit ? "" : "hidden"} w-80 border-2 border-green-700 px-2`} type="text" value={equestion} onChange={e => setEquestion(e.target.value)} />
-            <div className={`${!edit ? "" : "hidden"} w-80 border-2 border-black`}>{props.data.question.slice(0, 30)}</div>
+            <div className={`${!edit ? "" : "hidden"} w-80 p-1 sm:p-0 h-8 overflow-x-scroll sm:overflow-hidden sm:h-auto border-2 border-black`}>{props.data.question.slice(0, 30)}</div>
 
             <input className={`${edit ? "" : "hidden"} w-80 border-2 border-green-700 px-2`} type="text" value={eanswer} onChange={e => setEanswer(e.target.value)} />
-            <div className={`${!edit ? "" : "hidden"} w-80 border-2 border-black`}>{props.data.answer.slice(0, 30) + "..."}</div>
-            <div className='w-20 border-2 border-black' onClick={() => setEdit(!edit)}><i className="fa-regular fa-pen-to-square"></i></div>
-            <div className={`${!edit ? "" : "hidden"} w-20 border-2 border-black cursor-pointer`} onClick={() => props.func.f1(props.data.id)}><i className="fa-solid fa-trash-can" ></i></div>
+            <div className={`${!edit ? "" : "hidden"} w-80 p-1 sm:p-0  h-8 overflow-x-scroll sm:overflow-hidden sm:h-auto border-2 border-black`}>{props.data.answer.slice(0, 30) + "..."}</div>
+            <div className='hidden sm:inline w-20 border-2 border-black cursor-pointer' onClick={() => setEdit(!edit)}><i className="fa-regular fa-pen-to-square"></i></div>
+            <div className={`${!edit ? "" : "hidden"} w-20 p-1 sm:p-0 border-2 border-black cursor-pointer`} onClick={() => props.func.f1(props.data.id)}><i className="fa-solid fa-trash-can" ></i></div>
             <button className={`${edit ? "" : "hidden"} bg-black text-white w-20 tracking-widest border-2 border-black hover:bg-white hover:text-black`} onClick={editItem}>EDIT</button>
         </div>
     )
--- Frontend/src/Pages/Admin/Admin.jsx
+++ Frontend/src/Pages/Admin/Admin.jsx
@@ -9,10 +9,15 @@ const Admin = () => {
     const [answer, setAnswer] = useState("")
     const [topic, setTopic] = useState("")
     const [loader, setLoader] = useState(true)
+    const [change, setChange] = useState(0)
 
     useEffect(() => {
         getQuizItems()
-    }, [item])
+    }, [item, change])
+
+    const Edit = () => {
+        setChange(change + 1)
+    }
 
     const getQuizItems = async () => {
         setLoader(true)
@@ -34,35 +39,29 @@ const Admin = () => {
     }
     return (
         <div className='my-28 w-full flex justify-center'>
-            {loader ? <div className="loader"></div> : <div className='w-3/4'>
-                <div className='flex-col py-2'>
-                    <h4 className='font-bold text-3xl ml-4 mb-2'>Add a Question</h4>
-                    <form className='flex'>
-                        <input className='w-56 ml-4 p-1 mr-5 border-2 border-black' type="text" placeholder='Question' value={question} onChange={(e) => { setQuestion(e.target.value) }} />
-                        <input className='w-56 p-1 mr-5 border-2 border-black' type="text" placeholder='Answer' value={answer} onChange={(e) => { setAnswer(e.target.value) }} />
-                        <input className='w-56 p-1 mr-5 border-2 border-black' type="text" placeholder='Topic' value={topic} onChange={(e) => { setTopic(e.target.value) }} />
-                        <button className='border-2 w-32 border-black hover:text-white hover:bg-black' type="submit" onClick={addItem}>Add</button>
+            {loader ? <div className="loader"></div> : <div className='w-11/12 sm:w-3/4'>
+                <div className='w-full flex-col py-2'>
+                    <h4 className='font-bold text-3xl sm:ml-4 mb-2'>Add Question</h4>
+                    <form className='flex flex-col sm:flex-row justify-center items-center sm:justify-normal '>
+
+                        <input className='w-11/12 sm:w-56 mb-5 sm:mb-0 sm:ml-4 p-1 mr-5 border-2 border-black' type="text" placeholder='Question' value={question} onChange={(e) => { setQuestion(e.target.value) }} />
+                        <input className='w-11/12 sm:w-56 mb-5 sm:mb-0 p-1 mr-5 border-2 border-black' type="text" placeholder='Answer' value={answer} onChange={(e) => { setAnswer(e.target.value) }} />
+                        <input className='w-11/12 sm:w-56 mb-5 sm:mb-0 p-1 mr-5 border-2 border-black' type="text" placeholder='Topic' value={topic} onChange={(e) => { setTopic(e.target.value) }} />
+                        <button className='border-2 w-32 border-black hover:text-white hover:bg-black p-1 self-start sm:self-auto ml-1' type="submit" onClick={addItem}>Add</button>
                     </form>
                 </div>
 
-                <div className='flex-col mt-10'>
-                    <h4 className='font-bold text-3xl ml-4 mb-2'>Data Base</h4>
-                    <div className='flex ml-4 mb-4 text-center tracking-wider' style={{ display: "flex", gap: "15px" }}>
+                <div className='flex-col mt-10 w-full'>
+                    <h4 className='font-bold text-3xl sm:ml-4 mb-2'>Data Base</h4>
+                    <div className='flex sm:ml-4 mb-4 text-center tracking-wider' style={{ display: "flex", gap: "15px" }}>
 
                         <div className='w-20 p-1 bg-black text-white'>TOPIC</div>
                         <div className='w-80 p-1 bg-black text-white'>QUESTION</div>
                         <div className='w-80 p-1 bg-black text-white'>ANSWER</div>
-                        <div className='w-20 p-1 bg-black text-white'><i className="fa-regular fa-pen-to-square"></i></div>
+                        <div className='w-20 p-1 bg-black text-white hidden sm:inline'><i className="fa-regular fa-pen-to-square"></i></div>
                         <div className='w-20 p-1 bg-black text-white'><i className="fa-solid fa-trash-can" ></i></div>
                     </div>
-                    {items ? items.map(ele => <DB_Card data={ele} func={{ f1: deleteItem }} />
-                        // <div className='flex ml-4 mb-4 text-center' key={ele.id} style={{ display: "flex", gap: "15px" }}>
-                        //     <div className='w-20 border-2 border-black'>{ele.topic}</div>
-                        //     <div className='w-80 border-2 border-black'>{ele.question.slice(0, 30)}</div>
-                        //     <div className='w-80 border-2 border-black'>{ele.answer.slice(0, 30) + "..."}</div>
-                        //     <div className='w-20 border-2 border-black'><i className="fa-regular fa-pen-to-square"></i></div>
-                        //     <div className='w-20 border-2 border-black cursor-pointer' onClick={() => deleteItem(ele.id)}><i className="fa-solid fa-trash-can" ></i></div>
-                        // </div>
+                    {items ? items.map(ele => <DB_Card key={ele.id} data={ele} func={{ f1: deleteItem, f2: Edit }} />
                     ) : ""}
                 </div>
             </div>
